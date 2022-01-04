73420a04adca878e23a77e03671dc2b9b146c0cd
FFilipR <filip@gmail.com>
2022-01-04 12:34:30 +0100

    added ToColumnType and ToFieldType methods in Field Class.

---
+++ ORM.FrameWork/MetaModels/Field.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_FrameWork.MetaModels
+{
+    internal class Field
+    {
+        public Field(Entity entity)
+        {
+            this.Entity = entity;
+        }
+        public Entity Entity { get; internal set; } // entity who it belongs 
+        public MemberInfo Member { get; internal set; }
+
+        public Type Type // in Object
+        {
+            get
+            {
+                if (Member is PropertyInfo)
+                    return ((PropertyInfo)Member).PropertyType;
+
+                throw new NotSupportedException();
+            }
+        }
+        public string ColumnName { get; internal set; }
+        public Type ColumnType { get; internal set; } // on DB level
+        public bool IsNullable { get; internal set; } 
+        public bool IsPrimaryKey { get; internal set; } 
+        public bool IsForeignKey { get; internal set; }
+        public object GetValue (object obj)
+        {
+            if (Member is PropertyInfo)
+                return ((PropertyInfo)Member).GetValue(obj);
+
+            throw new NotSupportedException("Type of the member is not supported.");
+        }
+        public void SetValue(object obj, object val)
+        {
+            if (Member is PropertyInfo)
+                 ((PropertyInfo)Member).SetValue(obj, val);
+
+            throw new NotSupportedException("Type of the member is not supported.");
+        }
+
+        public object ToColumnType(object val) // taking an object type and converting it in the corresponding type in DB
+        {
+            if(IsForeignKey)
+            {
+                object obj = ORMapper.GetEntity(Type).PKey.GetValue(val);
+                return ORMapper.GetEntity(Type).PKey.ToColumnType(obj); // for example type is student datatype // Type.GetEntity() == ORMapper.GetEntity(Type)
+            }
+
+            //if (Type == ColumnType)
+            //    return val;
+
+            if (val is bool)
+            {
+                if (ColumnType == typeof(int))
+                    return ((bool) val) ? 1 : 0; // if cast true ist, return 1 else 0
+                if (ColumnType == typeof(short))
+                    return ((bool) val) ? 1 : 0;
+                if (ColumnType == typeof(long))
+                    return ((bool) val) ? 1 : 0;
+            }
+
+            return val;
+        }
+
+        public object ToFieldType (object val)
+        {
+            if (Type == typeof(bool))
+            {
+                if (val is int)
+                    return ((int) val) != 0;
+                if (val is long)
+                    return ((int) val) != 0;
+                if (val is short)
+                    return ((int) val) != 0;
+            }
+
+            if (Type == typeof(short))
+                return Convert.ToInt16(val);
+            if (Type == typeof(int))
+                return Convert.ToInt32(val);
+            if (Type == typeof(long))
+                return Convert.ToInt64(val);
+
+            if (Type.IsEnum)
+                return Enum.ToObject(Type, val);
+
+            return val;
+        }
+
+    }
+}
---
+++ ORM.FrameWork/ORMapper.cs
@@ -0,0 +1,25 @@
+﻿using ORM_FrameWork.MetaModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_FrameWork
+{
+    public class ORMapper
+    {
+        private static Dictionary<Type, Entity> Entities = new Dictionary<Type, Entity>();
+        public static IDbConnection DbConnection { get; set; } 
+        internal static Entity GetEntity(object obj) 
+        {
+            Type type = ((obj is Type) ? (Type) obj : obj.GetType()); // if obj then GetType , otherwise type
+
+            if (!Entities.ContainsKey(type)) 
+                Entities.Add(type, new Entity(type));
+              
+            return Entities[type];   // return value from dictionary by key
+        }
+    }
+}
